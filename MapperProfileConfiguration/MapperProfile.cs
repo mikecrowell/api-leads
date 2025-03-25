221d7de921c4875a747333b3fc031ca333a95251
Michael A Crowell <sayhi2mikec@yahoo.com>
2025-03-24 23:13:09 -0500

    Add files via upload

---
+++ MapperProfileConfiguration/MapperProfile.cs
@@ -0,0 +1,29 @@
+﻿using api.leads.Data.Models;
+using api.leads.DTO.Response;
+using AutoMapper;
+using System.Diagnostics.CodeAnalysis;
+
+namespace api.leads.MapperProfileConfiguration
+{
+    [ExcludeFromCodeCoverage]
+	public class MapperProfile : Profile
+	{
+		public MapperProfile()
+		{
+			CreateMap<LeadType, LeadTypeGetResponse>();
+			CreateMap<DuplicateLead, DuplicateLeadsGetResponse>();
+			CreateMap<Campaign, CampaignGetResponse>();
+			CreateMap<CampaignId, CampaignIdGetResponse>();
+			CreateMap<Charges, ChargesGetResponse>();
+			CreateMap<PossibleLeads, PossibleLeadsGetResponse>();
+			CreateMap<PurchasedLeads, PurchasedLeadsGetResponse>();
+			CreateMap<PurchaseLeads, PurchaseLeadsGetResponse>();
+			CreateMap<CampaignFields, CampaignFieldsGetResponse>();
+			CreateMap<Person, PersonGetResponse>();
+			CreateMap<Call, CallGetResponse>();
+			CreateMap<Forms, FormsGetResponse>();
+			CreateMap<Groups, GroupsGetResponse>();
+			CreateMap<LeadsToMatch, LeadsToMatchGetResponse>();
+		}
+	}
+}
